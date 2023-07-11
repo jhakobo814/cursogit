171230a62d63984e17a3d9a2a4b834f140e29ba1
Leonardo <jhkobo8@gmail.com>
2023-07-11 10:26:43 -0500

    comienzo el proyecto

---
+++ capitulo9_entrada_salida.py
@@ -0,0 +1,32 @@
+print("hola python")
+
+print("Estoy","Aprendiendo","Python")
+
+print(1,2,3)
+
+print("Hola Python",2019)
+
+texto =  "Hola Python"
+anio = 2020
+print(texto,anio)
+
+print(1,2,3)
+print(1,2,3, sep=",")
+print(1,2,3, sep="+")
+
+print(1,2,3,sep=",",end="-")
+print(1,2,3,sep=",",end="*")
+
+print("\n")
+print("Primera linea: \"texto entre comillas\"\nSegunda Linea: '\x24\'")
+print("\x4C")
+
+print(type("hola mundo"))
+print("leonardo " + "agudelo")
+
+
+print("Introduzca su nombre:")
+nombre = input()
+apellidos = input("Introduzca sus apellidos: ")
+print("Hola!",nombre,apellidos)
+
